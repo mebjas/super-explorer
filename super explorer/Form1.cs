0c323113fe796ef03fcaa4edd234bb617ef2cd6b
mebjas <minhazav@gmail.com>
2015-02-23 22:34:38 +0530

    added workspace, quicklinks and tree

--- super explorer/Form1.cs
+++ super explorer/Form1.cs
@@ -12,9 +12,37 @@ namespace super_explorer
 {
     public partial class main : Form
     {
+        public String[] quicklinks_data = {"Images", "Videos", "Movies", "Music", "Desktop", "Documents", "Mails"};
         public main()
         {
             InitializeComponent();
+            log("system loaded");
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            string[] drives = System.IO.Directory.GetLogicalDrives();
+            log("loading all logical drives");
+            int i = 0;
+            foreach (string str in drives)
+            {
+                tree.Nodes.Add(str);
+                workspace.Items.Add(str);
+                workspace.Items[i++].ImageIndex = 0;
+                log(str);
+            }
+
+            foreach (string s in quicklinks_data)
+            {
+                quicklinks.Items.Add(s);
+                log("added " +s +" to the quick links");
+            }
+        }
+
+        public void log(String m) {
+            console.Text += m + Environment.NewLine;
+            console.SelectionStart = console.Text.Length;
+            console.ScrollToCaret();
         }
     }
 }
---
+++ super explorer/notif.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace super_explorer
+{
+    public partial class notif : Form
+    {
+        public notif(String mesg, int category = globals.CAT_NORMAL)
+        {
+            InitializeComponent();
+            message.Text = mesg;
+            switch (category)
+            {
+                case globals.CAT_SUCCESS:
+                    this.BackColor = System.Drawing.Color.MediumSeaGreen;
+                    break;
+                case globals.CAT_WARNING:
+                    this.BackColor = System.Drawing.Color.Orange;
+                    break;
+                case globals.CAT_NORMAL:
+                default:
+                    this.BackColor = System.Drawing.Color.SkyBlue;
+                    break;
+            }
+            message.BackColor = this.BackColor;
+        }
+
+        private void notif_Load(object sender, EventArgs e)
+        {
+            Screen sobj = Screen.AllScreens[0];
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.Right > sobj.WorkingArea.Right)
+                    sobj = screen;
+            }
+
+            this.Left = sobj.WorkingArea.Right - this.Width;
+            this.Top = sobj.WorkingArea.Top;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class globals
+    {
+        public const int CAT_NORMAL = 0;
+        public const int CAT_SUCCESS = 1;
+        public const int CAT_WARNING = 2;
+    };
+}
