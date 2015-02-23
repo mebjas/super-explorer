639d112b34e8f9d39e54fe0625560c55f2ecb1d3
mebjas <minhazav@gmail.com>
2015-02-23 15:10:39 +0530

    initial code, and UI

---
+++ super explorer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace super_explorer
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.menu_box = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.url = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1,
+            this.menu_box});
+            this.shapeContainer1.Size = new System.Drawing.Size(1350, 699);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // menu_box
+            // 
+            this.menu_box.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.menu_box.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.menu_box.BorderColor = System.Drawing.Color.Transparent;
+            this.menu_box.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            this.menu_box.Location = new System.Drawing.Point(0, 0);
+            this.menu_box.Name = "menu_box";
+            this.menu_box.SelectionColor = System.Drawing.Color.Transparent;
+            this.menu_box.Size = new System.Drawing.Size(198, 698);
+            // 
+            // url
+            // 
+            this.url.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.url.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.url.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.url.Location = new System.Drawing.Point(1137, 12);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(201, 26);
+            this.url.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(446, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(685, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.SelectionColor = System.Drawing.Color.Transparent;
+            this.lineShape1.X1 = 198;
+            this.lineShape1.X2 = 1354;
+            this.lineShape1.Y1 = 52;
+            this.lineShape1.Y2 = 52;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 699);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "Computer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape menu_box;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.TextBox textBox1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+
+    }
+}
+
